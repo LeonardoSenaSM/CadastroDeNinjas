58a798c9a32ad3f0c22ff83ed1411ed3009c76ae
LeonardoSenaSM <leomagridf@gmail.com>
2025-08-25 15:13:54 -0300

    feat: change routes in controller and inject dependency service and a repository

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,9 +1,17 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/ninjas")
 public class NinjaController {
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
 
     //Adicionar Ninjas(CREATE)
     @PostMapping("/create")
@@ -13,14 +21,14 @@ public class NinjaController {
 
     //Mostrar todos os ninjas(READ)
     @GetMapping("/all")
-    public String mostrarNinja(){
-        return "Mostrar Ninjas";
+    public List<NinjaModel> listarNinjas(){
+        return ninjaService.listarNinjas();
         }
 
     //Procurar ninja por ID(READ
     @GetMapping("/allID")
-    public String mostrarNinjaPorID() {
-        return "Mostrar Ninja por ID";
+    public List<NinjaModel> mostrarNinjaPorID(Long id) {
+        return ninjaService.listarNinjasPorID(id);
         }
 
     //Alterar dados dos ninjas(UPDATE)
